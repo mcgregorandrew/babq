45657d6f317bd194b42ca5fc01149927e0f6686b
Andrew McGregor <mcgregorjohnandrew@gmail.com>
2020-09-17 19:53:06 -0400

    Simplied prepared statement to support debugging

--- src/com/bronzeage/babq/db/BabqDbPatient.java
+++ src/com/bronzeage/babq/db/BabqDbPatient.java
@@ -38,55 +38,55 @@ public class BabqDbPatient extends BabqDbBase {
 			prep = makePrepStmt(cmd);
 			int offset = 0;
 			for (String tokens[]: lines) {
-				prep.setInt(1+offset, Integer.parseInt(tokens[0])); // pt num
-				setString(prep, 2+offset, tokens[1]); // surname
-				setString(prep, 3+offset, tokens[4]); // first name
+				prep.setInt(++offset, Integer.parseInt(tokens[0])); // pt num
+				setString(prep, ++offset, tokens[1]); // surname
+				setString(prep, ++offset, tokens[4]); // first name
 				
 				//Changed in version 5.0
 				//Changed in version 6.0
-				setString(prep, 4+offset, tokens[69]); // health card province
+				setString(prep, ++offset, tokens[69]); // health card province
 				
 				/**
 				 * @Since version 3.1. Address address to clinic output data
 				 */
-				setString(prep, 5+offset, tokens[8]); // mailing address line 1
-				setString(prep, 6+offset, tokens[9]); // mailing address line 2
-				setString(prep, 7+offset, tokens[12]); // mailing city
-				setString(prep, 8+offset, tokens[13]); // mailing province
-				setString(prep, 9+offset, tokens[14]); // mailing country
-				setString(prep, 10+offset, tokens[15]); // mailing postal code
+				setString(prep, ++offset, tokens[8]); // mailing address line 1
+				setString(prep, ++offset, tokens[9]); // mailing address line 2
+				setString(prep, ++offset, tokens[12]); // mailing city
+				setString(prep, ++offset, tokens[13]); // mailing province
+				setString(prep, ++offset, tokens[14]); // mailing country
+				setString(prep, ++offset, tokens[15]); // mailing postal code
 				
 				/**
 				 * @Since version 3.1. Address address to clinic output data
 				 */
-				setString(prep, 11+offset, tokens[20]); // residential address line 1
-				setString(prep, 12+offset, tokens[21]); // residential address line 2
-				setString(prep, 13+offset, tokens[24]); // residential city
-				setString(prep, 14+offset, tokens[25]); // residential province
-				setString(prep, 15+offset, tokens[26]); // residential country
-				setString(prep, 16+offset, tokens[27]); // residential postal code
+				setString(prep, ++offset, tokens[20]); // residential address line 1
+				setString(prep, ++offset, tokens[21]); // residential address line 2
+				setString(prep, ++offset, tokens[24]); // residential city
+				setString(prep, ++offset, tokens[25]); // residential province
+				setString(prep, ++offset, tokens[26]); // residential country
+				setString(prep, ++offset, tokens[27]); // residential postal code
 				
-				prep.setDate(17+offset, DateFormatter.stringToDate(tokens[31])); // Birth Date
-				setString(prep, 18+offset, tokens[32]); //Sex
+				prep.setDate(++offset, DateFormatter.stringToDate(tokens[31])); // Birth Date
+				setString(prep, ++offset, tokens[32]); //Sex
 				
 				//Changed in version 5.0
 				//Changed in version 6.0
-				setString(prep, 19+offset, tokens[70]); // Health Card Number
-				setString(prep, 20+offset, tokens[72]); // Health Card Version Code
+				setString(prep, ++offset, tokens[70]); // Health Card Number
+				setString(prep, ++offset, tokens[72]); // Health Card Version Code
 				Date expiryDate = DateFormatter.stringToDate(tokens[74]); // Card Expire Date
-				prep.setDate(21+offset, expiryDate);
+				prep.setDate(++offset, expiryDate);
 				if ((expiryDate == null)
 						|| (BabqUtils.isLastDayOfMonth(expiryDate.getTime()))) {
-					prep.setDate(22+offset, expiryDate);
+					prep.setDate(++offset, expiryDate);
 				} else {
-					prep.setDate(22+offset, new Date(BabqUtils
+					prep.setDate(++offset, new Date(BabqUtils
 							.getDateAtLastDayOfMonth(expiryDate.getTime())));
 				}
 	
 				//Changed in version 5.0
 				String familyMd = tokens[52]; // Family Doctor
 				try {
-					prep.setInt(23+offset, Integer.parseInt(familyMd));
+					prep.setInt(++offset, Integer.parseInt(familyMd));
 				} catch (NumberFormatException e) {
 					if (familyMd.trim().length() != 0)
 						warningList
@@ -95,18 +95,18 @@ public class BabqDbPatient extends BabqDbBase {
 										"Error in FamilyMD number (found "
 												+ familyMd
 												+ ") - may need to re-export with references");
-					prep.setObject(23+offset, null);
+					prep.setObject(offset, null);
 				}
 				
-				prep.setString(24+offset, tokens[57]); // Doc Num
+				prep.setString(++offset, tokens[57]); // Doc Num
 	
-				setString(prep, 25+offset, tokens[33]); // mDeleted
+				setString(prep, ++offset, tokens[33]); // mDeleted
 				
 				//Changed in version 6.0
-				setString(prep, 26+offset, tokens[79]); // mMember status
-				prep.setString(27+offset, source);
-				prep.setInt(28+offset, lineNumber);
-				offset += 28;
+				setString(prep, ++offset, tokens[79]); // mMember status
+				prep.setString(++offset, source);
+				prep.setInt(++offset, lineNumber);
+
 				//System.out.println("Patient number: "+tokens[0]+" patient name: "+ tokens[4]+" "+tokens[1]+" memberStatus: "+tokens[78]);
 				
 	//			logger_m.info("PatientNum Surname FirstName Province MailProvince DateOfBirth Sex HealthNumber HNVersionCode DateOfCardExpiry DateOfCardExpiryCor " +
